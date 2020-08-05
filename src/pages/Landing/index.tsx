53cb42fd096108ac366a1f6918cf6cd7ac40c3dd
Raphael Costa <rafa.costa0@hotmail.com>
2020-08-05 12:01:46 -0300

    adding api call

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -7,6 +7,8 @@ import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import api from '../../services/api';
 
 import {
   Container,
@@ -18,6 +20,16 @@ import {
 } from './styles';
 
 const Landing = () => {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('/connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    })
+  }, []);
+
   return (
     <Container>
       <WebContentContainer>
@@ -42,7 +54,7 @@ const Landing = () => {
         </ButtonsContainer>
 
         <TotalConnections>
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+          Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
         </TotalConnections>
       </WebContentContainer>
     </Container>
