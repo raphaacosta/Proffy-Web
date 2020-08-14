b8fc7202657f5d2509f5ddd2ea82ac38d2b4ffbb
Raphael Costa <rafa.costa0@hotmail.com>
2020-08-14 14:59:02 -0300

    adding forgot password route

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,6 +6,7 @@ import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
 import Login from './pages/Login';
 import CreateUsers from './pages/CreateUsers';
+import ForgotPassword from './pages/ForgotPassword';
 
 const Routes: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const Routes: React.FC = () => {
       <Route path="/" component={Landing} exact/>
       <Route path="/login" component={Login} />
       <Route path="/users" component={CreateUsers} />
+      <Route path="/forgot_password" component={ForgotPassword} />
       <Route path="/study" component={TeacherList} />
       <Route path="/give-classes" component={TeacherForm} />
     </BrowserRouter>
