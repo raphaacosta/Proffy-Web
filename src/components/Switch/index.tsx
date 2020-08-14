6ba96a5c735850cc4ddc49bf7fe6084637b2f0a0
Raphael Costa <rafa.costa0@hotmail.com>
2020-08-13 22:19:47 -0300

    updating switch component

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 
-import { SwitchContainer } from './styles';
 import Switch from 'react-switch';
 import { ThemeContext } from 'styled-components';
 import { useTheme } from '../../hooks/theme';
@@ -12,23 +11,21 @@ const SwitchComponent: React.FC = () => {
   const { toggleTheme } = useTheme();
 
   return (
-    <SwitchContainer>
-      <Switch 
-          className="switch"
-          onChange={toggleTheme}
-          checked={title === 'dark'}
-          checkedIcon={<span>
-            <FaMoon color="#202040" style={{marginTop: 5, marginLeft: 8,}}/>
-          </span>}
-          uncheckedIcon={<span>
-            <FaSun color="#6633CC" style={{marginTop: 5, marginLeft: 5,}}/>
-          </span>}
-          offColor="#202040"
-          onColor="#602080"
-          offHandleColor="#602080"
-          onHandleColor="#202040"
-      />
-    </SwitchContainer>
+    <Switch 
+        className="switch"
+        onChange={toggleTheme}
+        checked={title === 'dark'}
+        checkedIcon={<span>
+          <FaMoon color="#202040" style={{marginTop: 5, marginLeft: 8,}}/>
+        </span>}
+        uncheckedIcon={<span>
+          <FaSun color="#6633CC" style={{marginTop: 5, marginLeft: 5,}}/>
+        </span>}
+        offColor="#202040"
+        onColor="#602080"
+        offHandleColor="#602080"
+        onHandleColor="#202040"
+    />
   );
 }
 
