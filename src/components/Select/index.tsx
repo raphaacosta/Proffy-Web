f64c9cfedc5aade85d410832efbf6cc47c2aae8b
Raphael Costa <rafa.costa0@hotmail.com>
2020-08-16 20:37:42 -0300

    updating option text

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -16,7 +16,7 @@ const Select: React.FC<SelectProps> = ({ label, name, options,...rest }) => {
     <SelectBlock className="select-block">
       <label htmlFor={name}>{label}</label>
       <select value="" id={name} {...rest}>
-        <option value="" disabled hidden>Selecione uma opção</option>
+        <option value="" disabled hidden>Selecione qual você quer ensinar</option>
 
         {options.map(option => {
           return <option key={option.value} value={option.value}>{option.label}</option>
